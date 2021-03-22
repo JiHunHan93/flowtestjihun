734b4e4c405ca4320ec2c63825459476122b7b61
DESKTOP-O4NMRST\epyi1 <epyi93@gmail.com>
2021-03-23 00:12:48 +0900

    유진 수정
    최초수정

---
+++ src/com/jihunh/spring/eugeneyi/EugeneYi.java
@@ -0,0 +1,18 @@
+package com.jihunh.spring.eugeneyi;
+
+public class EugeneYi {
+
+	public static void main(String[] args) {
+		
+		String Hello1 = "¾È";
+		String Hello2 = "³ç";
+		String Hello3 = "ÇÏ";
+		String Hello4 = "¼¼";
+		String Hello5 = "¿ä";
+		String Hello6 = "!";
+		
+		System.out.println(Hello1 + Hello2 + Hello3 + Hello4 + Hello5 + Hello6);
+
+	}
+
+}
